aee805993461e97a957b69cce2b1a61a76af1e69
yuminhwan <ymh7532@icloud.com>
2022-05-01 18:13:41 +0900

    lecture: 코드 정리

--- src/lecture/string/Main_03.java
+++ src/lecture/string/Main_03.java
@@ -1,16 +1,37 @@
 package lecture.string;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main_03_3 {
+public class Main_03 {
     public static void main(String[] args) {
-        Main_03_3 t = new Main_03_3();
+        Main_03 t = new Main_03();
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
         System.out.println(t.soultion(str));
     }
 
     public String soultion(String str) {
+        String[] arr = str.split(" ");
+        Arrays.sort(arr, (o1, o2) -> o2.length() - o1.length());
+        return arr[0];
+    }
+
+    public String soultion2(String str) {
+        String answer = "";
+        int max = Integer.MIN_VALUE;
+        String[] arr = str.split(" ");
+        for (String s : arr) {
+            int len = s.length();
+            if (len > max) {
+                max = len;
+                answer = s;
+            }
+        }
+        return answer;
+    }
+
+    public String soultion3(String str) {
         String answer = "";
         int max = Integer.MIN_VALUE;
         int pos;
