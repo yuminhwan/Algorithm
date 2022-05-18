826e9bbf6640a25351494069487a954ec5e7f187
yuminhwan <ymh7532@icloud.com>
2022-05-18 12:47:34 +0900

    array: 11. 뒤집은 소수, 12. 멘토링

---
+++ src/lecture/array/Main_11.java
@@ -0,0 +1,40 @@
+package lecture.array;
+
+import java.util.Scanner;
+
+public class Main_11 {
+    public static void main(String[] args) {
+        Main_11 t = new Main_11();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] arr = new int[n + 1][6];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 5; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println(t.solution(n, arr));
+    }
+
+    private int solution(int n, int[][] arr) {
+        int answer = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= 5; k++) {
+                    if (arr[i][k] == arr[j][k]) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if (cnt > max) {
+                max = cnt;
+                answer = i;
+            }
+        }
+        return answer;
+    }
+
+}
---
+++ src/lecture/array/Main_12.java
@@ -0,0 +1,48 @@
+package lecture.array;
+
+import java.util.Scanner;
+
+public class Main_12 {
+    public static void main(String[] args) {
+        Main_12 t = new Main_12();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println(t.solution(n, m, arr));
+    }
+
+    private int solution(int n, int m, int[][] arr) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int cnt = 0;
+                for (int k = 0; k < m; k++) {
+                    int pi = 0;
+                    int pj = 0;
+                    for (int s = 0; s < n; s++) {
+                        if (arr[k][s] == i) {
+                            pi = s;
+                        }
+                        if (arr[k][s] == j) {
+                            pj = s;
+                        }
+                    }
+                    if (pi < pj) {
+                        cnt++;
+                    }
+                }
+                if (cnt == m) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
+}
