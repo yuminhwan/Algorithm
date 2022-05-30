d441fee63da23a82ded46a13f91891f276ab0733
yuminhwan <ymh7532@icloud.com>
2022-05-30 21:01:43 +0900

    stack&queue : 1. 올바른 괄호

---
+++ src/lecture/stack_queue/Main_01.java
@@ -0,0 +1,34 @@
+package lecture.stack_queue;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        System.out.println(solution(str));
+    }
+
+    private static String solution(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            if (c == '(') {
+                stack.add(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return "NO";
+                }
+                stack.pop();
+            }
+        }
+
+        if (!stack.isEmpty()) {
+            return "NO";
+        }
+
+        return "YES";
+    }
+
+}
