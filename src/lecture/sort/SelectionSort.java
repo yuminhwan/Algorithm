e1279c556f721e86b6ee6d6f6088b54a5dc109e3
yuminhwan <ymh7532@icloud.com>
2022-06-03 14:06:52 +0900

    sort : 선택 정렬

---
+++ src/lecture/sort/SelectionSort.java
@@ -0,0 +1,32 @@
+package lecture.sort;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        for (int x : solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+
+    private static int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int tmp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = tmp;
+        }
+        return arr;
+    }
+}
