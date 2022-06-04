3c701f98a8f348b59d4a6f232e5b58ae4394773a
yuminhwan <ymh7532@icloud.com>
2022-06-04 22:52:34 +0900

    sort : 버블 정렬

---
+++ src/lecture/sort/BubbleSort.java
@@ -0,0 +1,44 @@
+package lecture.sort;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        for (int x : solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+
+    private static int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n - i; j++) {
+                if (arr[j - 1] > arr[j]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    private static int[] solution2(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+}
