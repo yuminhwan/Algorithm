cad9f86fe90d43f625f37fea01b757c54f34723d
yuminhwan <ymh7532@icloud.com>
2022-06-08 17:31:52 +0900

    sort : 퀵 정렬 구현

--- src/lecture/sort/Main_05.java
+++ src/lecture/sort/Main_05.java
@@ -17,7 +17,6 @@ public class Main_05 {
     }
 
     private static String solution(int n, int[] arr) {
-        Arrays.asList(arr);
         Set<Integer> set = new HashSet<>();
         for (int x : arr) {
             if (!set.add(x)) {
@@ -36,6 +35,59 @@ public class Main_05 {
             }
         }
         return "U";
+    }
+
+    private static String solution3(int n, int[] arr) {
+        quickSort(arr, 0, n - 1);
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                return "D";
+            }
+        }
+        return "U";
+    }
+
+    private static void quickSort(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        int pivot = partition(arr, left, right);
+
+        quickSort(arr, left, pivot - 1);
+        quickSort(arr, pivot + 1, right);
+    }
+
+    private static int partition(int[] arr, int left, int right) {
+
+        int mid = (left + right) / 2;
+        swap(arr, left, mid);
+
+        int pivot = arr[left];
+        int lo = left;
+        int hi = right;
+
+        while (lo < hi) {
+            while (pivot < arr[hi] && lo < hi) {
+                hi--;
+            }
+
+            while (pivot >= arr[lo] && lo < hi) {
+                lo++;
+            }
+
+            swap(arr, lo, hi);
+        }
+
+        swap(arr, left, lo);
+        return lo;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
 }
