1ce3de7aed675f5997f8583b5c1c23e1eba19315
yuminhwan <ymh7532@icloud.com>
2022-06-17 22:48:43 +0900

    recursive : 4. 피보나치 재귀 (메모이제이션)

--- src/lecture/recursive/Main_04.java
+++ src/lecture/recursive/Main_04.java
@@ -10,7 +10,7 @@ public class Main_04 {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         fibo = new int[n + 1];
-        dfs(n);
+        dfs4(n);
         for (int i = 1; i <= n; i++) {
             System.out.print(fibo[i] + " ");
         }
@@ -21,6 +21,32 @@ public class Main_04 {
             return fibo[n] = 1;
         }
         return fibo[n] = dfs(n - 2) + dfs(n - 1);
+    }
+
+    private static int dfs2(int n) {
+        if (fibo[n] > 0) {
+            return fibo[n];
+        }
+
+        if (n <= 2) {
+            return fibo[n] = 1;
+        }
+        return fibo[n] = dfs2(n - 2) + dfs2(n - 1);
+    }
+
+    private static void dfs3(int n) {
+        fibo[1] = 1;
+        fibo[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+    }
+
+    private static int dfs4(int n) {
+        if (n <= 2) {
+            return 1;
+        }
+        return dfs4(n - 1) + dfs4(n - 2);
     }
 
 }
