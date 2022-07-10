bc271a3d06e78a2e2c42158cc945550d6ccfa74c
yuminhwan <ymh7532@icloud.com>
2022-07-10 23:32:54 +0900

    DFS: 12. 경로탐색(인접행렬)

---
+++ src/lecture/recursive/Main_12_01.java
@@ -0,0 +1,43 @@
+package lecture.recursive;
+
+import java.util.Scanner;
+
+public class Main_12_01 {
+
+    static int[][] adj;
+    static boolean[] vistied;
+    static int n, m, answer;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        m = scanner.nextInt();
+
+        adj = new int[n + 1][n + 1];
+
+        for (int i = 0; i < m; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            adj[x][y] = 1;
+        }
+
+        vistied = new boolean[n + 1];
+        dfs(1);
+        System.out.println(answer);
+    }
+
+    public static void dfs(int x) {
+        vistied[x] = true;
+        if (x == n) {
+            answer++;
+        } else {
+            for (int y = 1; y < n + 1; y++) {
+                if (adj[x][y] == 1 && !vistied[y]) {
+                    vistied[y] = true;
+                    dfs(y);
+                    vistied[y] = false;
+                }
+            }
+        }
+    }
+}
