08193c887c401b32b2bddccdf273b6d3344cdf77
yuminhwan <ymh7532@icloud.com>
2022-07-19 23:59:49 +0900

    DFS & BFS: 13. 섬나라 아일랜드 bfs 추가

--- src/lecture/recursive/dfs_bfs/Main_13_bfs.java
+++ src/lecture/recursive/dfs_bfs/Main_13_bfs.java
@@ -3,9 +3,11 @@ package lecture.recursive.dfs_bfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
-public class Main_13 {
+public class Main_13_bfs {
 
 	public static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 	public static int n, answer;
@@ -26,7 +28,7 @@ public class Main_13 {
 			for (int j = 0; j < n; j++) {
 				if (arr[i][j] == 1) {
 					answer++;
-					dfs(i, j);
+					bfs(i, j);
 				}
 			}
 		}
@@ -34,14 +36,29 @@ public class Main_13 {
 		System.out.println(answer);
 	}
 
-	private static void dfs(int x, int y) {
-		arr[x][y] = 0;
-		for (int i = 0; i < 8; i++) {
-			int nx = x + move[i][0];
-			int ny = y + move[i][1];
-			if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] == 1) {
-				dfs(nx, ny);
+	private static void bfs(int x, int y) {
+		Deque<Point> deque = new ArrayDeque<>();
+		deque.add(new Point(x, y));
+		while (!deque.isEmpty()) {
+			Point point = deque.remove();
+			for (int i = 0; i < 8; i++) {
+				int nx = move[i][0] + point.x;
+				int ny = move[i][1] + point.y;
+				if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] == 1) {
+					arr[nx][ny] = 0;
+					deque.add(new Point(nx, ny));
+				}
 			}
+		}
+	}
+
+	public static class Point {
+		int x;
+		int y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
 		}
 	}
 
---
+++ src/lecture/recursive/dfs_bfs/Main_13_dfs.java
@@ -0,0 +1,48 @@
+package lecture.recursive.dfs_bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_13_dfs {
+
+	public static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	public static int n, answer;
+	public static int[][] arr;
+	public static int[][] move = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {-1, 1}, {1, -1}};
+
+	public static void main(String[] args) throws IOException {
+		n = Integer.parseInt(bf.readLine());
+		arr = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (arr[i][j] == 1) {
+					answer++;
+					dfs(i, j);
+				}
+			}
+		}
+
+		System.out.println(answer);
+	}
+
+	private static void dfs(int x, int y) {
+		arr[x][y] = 0;
+		for (int i = 0; i < 8; i++) {
+			int nx = x + move[i][0];
+			int ny = y + move[i][1];
+			if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] == 1) {
+				dfs(nx, ny);
+			}
+		}
+	}
+
+}
