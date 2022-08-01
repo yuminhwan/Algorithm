769be2045bb3df78390a82224ba7752d4f3e24aa
yuminhwan <ymh7532@icloud.com>
2022-08-01 09:34:44 +0900

    Dp: 2. 돌다리 건너기

---
+++ src/lecture/dp/Main_02.java
@@ -0,0 +1,38 @@
+package lecture.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_02 {
+
+    public static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(bf.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(solution(n, arr));
+    }
+
+    private static int solution(int n, int[] arr) {
+        int answer = 0;
+        int[] dy = new int[n];
+        dy[0] = 1;
+        for (int i = 1; i < n; i++) {
+            int max = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] < arr[i] && dy[j] > max) {
+                    max = dy[j];
+                }
+            }
+            dy[i] = max + 1;
+            answer = Math.max(answer, dy[i]);
+        }
+        return answer;
+    }
+}
