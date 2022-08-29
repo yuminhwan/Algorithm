b0ae82ca1bf6b27300ed3711ec7f67244ae60751
Minhwan Yu <ymh7532@icloud.com>
2022-08-29 16:04:40 +0900

    [Silver V] Title: 소수, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/2581. 소수/소수.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        int min = -1;
+
+        for (int i = m; i < n + 1; i++) {
+            boolean isPrime = true;
+            if (i == 1) {
+                continue;
+            }
+
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                sum += i;
+                if (min == -1) {
+                    min = i;
+                }
+            }
+        }
+
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+
+    }
+}
+
