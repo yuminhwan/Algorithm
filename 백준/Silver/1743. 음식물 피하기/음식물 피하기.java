1447e8cc1b572de04ceec5e678984771536db026
Minhwan Yu <ymh7532@icloud.com>
2022-09-17 03:20:32 +0900

    [Silver I] Title: 음식물 피하기, Time: 164 ms, Memory: 15908 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,6 +9,7 @@ public class Main {
     static int N, M, K;
     static int[][] adj;
     static int[][] move = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    static int trashLength;
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
@@ -36,7 +35,8 @@ public class Main {
         for (int i = 1; i < N + 1; i++) {
             for (int j = 1; j < M + 1; j++) {
                 if (adj[i][j] == TRASH) {
-                    int trashLength = bfs(i, j);
+                    trashLength = 0;
+                    dfs(i, j);
                     max = Math.max(trashLength, max);
                 }
             }
@@ -44,40 +44,21 @@ public class Main {
         return max;
     }
 
-    private int bfs(int x, int y) {
-        Queue<Location> q = new LinkedList<>();
+    private void dfs(int x, int y) {
         adj[x][y] = 0;
-        q.add(new Location(x, y));
+        trashLength++;
 
-        int trashCount = 1;
-        while (!q.isEmpty()) {
-            Location cur = q.remove();
+        for (int i = 0; i < 4; i++) {
+            int dx = x + move[i][0];
+            int dy = y + move[i][1];
 
-            for (int i = 0; i < 4; i++) {
-                int dx = cur.x + move[i][0];
-                int dy = cur.y + move[i][1];
-
-                if (isValidRange(dx, dy) && adj[dx][dy] == TRASH) {
-                    trashCount++;
-                    adj[dx][dy] = 0;
-                    q.add(new Location(dx, dy));
-                }
+            if (isValidRange(dx, dy) && adj[dx][dy] == 1) {
+                dfs(dx, dy);
             }
         }
-        return trashCount;
     }
 
     private boolean isValidRange(int x, int y) {
         return 0 < x && 0 < y && x <= N && y <= M;
-    }
-
-    static class Location {
-        int x;
-        int y;
-
-        public Location(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
     }
 }
