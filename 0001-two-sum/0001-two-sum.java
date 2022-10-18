f1abda9d1b3c73021fdbb74190e2e0a551240003
Minhwan Yu <ymh7532@icloud.com>
2022-10-18 18:47:28 +0900

    Time: 4 ms (86.98%), Space: 45.8 MB (27.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,15 +4,11 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i<nums.length; i++) {
-            map.put(nums[i], i);
-        }
-        
-        for(int i = 0; i< nums.length; i++) {
             int complement = target - nums[i];
-            
-            if(map.containsKey(complement) && map.get(complement) != i) {
-                return new int[]{i, map.get(complement)};
+            if(map.containsKey(complement)) {
+                return new int[]{map.get(complement) , i};
             }
+            map.put(nums[i], i);
         }
         return null;
     }
