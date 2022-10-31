96d1c379af2739e05ff17685de35329c0dfabcf3
Minhwan Yu <ymh7532@icloud.com>
2022-11-01 00:31:21 +0900

    Time: 0 ms (100.00%), Space: 39.1 MB (90.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,20 @@
+/**
+* 시간복잡도 : O(N)
+* 공간복잡도 : O(1)
+*/
 class Solution {
     public int climbStairs(int n) {
-        if(n == 1 || n == 2){
+        if(n == 1){
             return n;
         }
         
-        
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
-        
+        int oneStep = 1;
+        int twoStep = 2;
         for(int i = 3; i<=n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            int tmp = twoStep; 
+            twoStep += oneStep; 
+            oneStep = tmp;
         }
-        
-        return dp[n];
+        return twoStep;
     }
 }
