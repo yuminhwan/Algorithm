e534b95f51a2097ad4bf0cc6da576594df4a25da
Minhwan Yu <ymh7532@icloud.com>
2022-11-01 00:11:36 +0900

    Time: 2 ms (84.94%), Space: 41.2 MB (64.99%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,15 +1,30 @@
 /** 
 * privious <= x루트 
 * privious^2 <= x 
-* 시간복잡도 : privious -> 0 ~ x루트 까지 O(x루트) 
+* 시간복잡도 : privious -> 0 ~ x루트까지 값 중 이분탐색 -> O(log(x루트))
 * 공간복잡도 : O(1)
 */
 class Solution {
     public int mySqrt(int x) {
-        long privious = 0;   
-        while((privious * privious) <= x) { 
-            privious++;
+        if(x == 0) {
+            return 0;
         }
-        return (int)privious - 1;
+        
+        int left = 1;
+        int right = x;
+        
+        while(left <= right) {
+            int mid = left + (right - left) / 2; 
+            
+            if(mid <= x/mid && (mid + 1) > x / (mid + 1)) {
+                return mid; 
+            } else if(mid <= x/mid){
+                left = mid + 1; 
+            } else {
+                right = mid - 1;
+            }
+            
+        }
+        return left;
     }
 }
